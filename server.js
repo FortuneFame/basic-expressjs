db4b0ba93713dda7458b2f2e8b32e739d9bec147
Marchenko Yaroslava <fortunefame1710@gmail.com>
2023-09-27 07:02:45 +0200

    served files (html, css, svg) using static express

--- server.js
+++ server.js
@@ -3,26 +3,11 @@ const path = require('path')
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/index.html'))
-});
-
-app.get('/about', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/about.html'))
-})
-
-app.get('/style.css', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public', 'style.css'))
-})
-
-app.get('/Expressjs.svg', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'public', 'Expressjs.svg'))
-})
+app.use(express.static('./public'))
 
 app.all('*', (req, res) => {
     res.status(404).sendFile(path.resolve(__dirname, './public/404.html'))
 })
-
 
 app.listen(PORT, () => {
     console.log(`Server is running: http://localhost:${PORT}/`)
